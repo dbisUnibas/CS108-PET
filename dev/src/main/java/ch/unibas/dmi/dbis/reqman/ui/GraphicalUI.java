a69efea486447bd9c399cac749bc851a0e420e56
loris.sauter <loris.sauter@unibas.ch>
2017-02-08 23:38:38 +0100

    Fixed IDE related issues. Added more experimental code to the GraphicalUI class

--- dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/GraphicalUI.java
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/GraphicalUI.java
@@ -6,13 +6,17 @@ package ch.unibas.dmi.dbis.reqman.ui;
  */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class GraphicalUI extends Application {
@@ -37,8 +41,22 @@ public class GraphicalUI extends Application {
         Text scenetitle = new Text("Test");
         grid.add(scenetitle, 0,0,2,1);
         Label userName = new Label("User Name: ");
+        grid.add(userName, 0, 1);
         TextField userTextField = new TextField();
         grid.add(userTextField, 1,1);
+        Label info = new Label("Bounds: ");
+        grid.add(info, 0, 2);
+        TextField infoBounds = new TextField();
+        infoBounds.setEditable(false);
+        Screen primary = Screen.getPrimary();
+        infoBounds.setText(primary.getVisualBounds().toString() );
+        grid.add(infoBounds, 1, 2);
+        Button button = new Button("Exit");
+        grid.add(button, 1, 3);
+        button.setOnAction((event) -> {
+            System.exit(0);
+        });
+
 
 
         primaryStage.show();
