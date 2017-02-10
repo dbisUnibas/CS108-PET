53988805948d49fca90893eef10330ff4aee91d8
sauterl <loris.sauter@gmail.com>
2017-02-10 15:36:23 +0100

    Added a UI utilities class

---
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/editor/UIUtilities.java
@@ -0,0 +1,24 @@
+package ch.unibas.dmi.dbis.reqman.ui.editor;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
+
+/**
+ * TODO: write JavaDoc
+ *
+ * @author loris.sauter
+ */
+public class UIUtilities {
+
+    /**
+     * Creates the widely used {@link GridPane} with its default styling.
+     * @return
+     */
+    public static GridPane generateDefaultGridPane() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setStyle("-fx-padding: 10px; -fx-spacing: 10px; -fx-hgap: 10px;-fx-vgap: 10px");
+        return grid;
+    }
+
+}
