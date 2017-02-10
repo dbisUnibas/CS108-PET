82e9b30420378bdba97e07204c1896d4c9356074
sauterl <loris.sauter@gmail.com>
2017-02-10 14:45:17 +0100

    Introduced shortcut methods to handle save and cancel actions on SaveCancelPane

--- dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/editor/SaveCancelPane.java
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/editor/SaveCancelPane.java
@@ -1,18 +1,21 @@
 package ch.unibas.dmi.dbis.reqman.ui.editor;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
  * Represents a component which has a save and cancel button.
- *
+ * <p>
  * The buttons are laid out to be right-aligned. To handle the {@link javafx.event.ActionEvent} fired
  * by the buttons, add a {@link SaveCancelHandler} to an instance of this class.
- *
+ * <p>
  * The class is derived from {@link AnchorPane} to directly add it to its parent(s). Although technical possible
  * it is not recommended to add other children to this pane.
  *
@@ -31,10 +34,13 @@ public class SaveCancelPane extends AnchorPane {
 
     private ArrayList<SaveCancelHandler> handlers = new ArrayList<>();
 
+    private EventHandler<ActionEvent> saveHandler = null;
+    private EventHandler<ActionEvent> cancelHandler = null;
+
     /**
      * Creates a new {@link SaveCancelPane} with its components laid out.
      */
-    public SaveCancelPane(){
+    public SaveCancelPane() {
         super();
         initComponents();
     }
@@ -42,35 +48,76 @@ public class SaveCancelPane extends AnchorPane {
     /**
      * Registers a {@link SaveCancelHandler} to the buttons of this instance.
      * May more than one handler is registered, they will be notified respecting FIFO principle.
+     *
      * @param handler The handler which handles the events fired by the buttons
      */
-    public void addSaveCancelHandler(SaveCancelHandler handler){
+    public void addSaveCancelHandler(SaveCancelHandler handler) {
         handlers.add(handler);
     }
 
     /**
      * Removes a {@link SaveCancelHandler} from the list of registered handlers.
+     *
      * @param handler The handler to remove.
      */
-    public void removeSaveCancelHandler(SaveCancelHandler handler){
+    public void removeSaveCancelHandler(SaveCancelHandler handler) {
         handlers.remove(handler);
+    }
+
+    /**
+     * Sets the handler for the save button.
+     * <p>
+     * This replaces any pre-registered {@link SaveCancelHandler}s.
+     *
+     * @param handler The handler to handle the save {@link ActionEvent}
+     */
+    public void setOnSave(EventHandler<ActionEvent> handler) {
+        this.saveHandler = handler;
+    }
+
+    /**
+     * Sets the handler for the cancel button.
+     * <p>
+     * This replaces any pre-registered {@link SaveCancelHandler}s.
+     *
+     * @param handler The handler to handle the cancel {@link ActionEvent}
+     */
+    public void setOnCancel(EventHandler<ActionEvent> handler) {
+        this.cancelHandler = handler;
+    }
+
+    private void handleSave(ActionEvent event) {
+        if (saveHandler != null) {
+            saveHandler.handle(event);
+        } else {
+            handlers.forEach(handler -> {
+                handler.save(event);
+            });
+        }
+    }
+
+
+    private void handleCancel(ActionEvent event) {
+        if (cancelHandler != null) {
+            cancelHandler.handle(event);
+        } else {
+            handlers.forEach(handler -> {
+                handler.cancel(event);
+            });
+        }
     }
 
     /**
      * Initializes the components.
      */
-    protected void initComponents(){
+    protected void initComponents() {
         // Init behavior
         saveButton.setOnAction(event -> {
-            handlers.forEach( handler -> {
-                handler.save(event);
-            });
+            handleSave(event);
             event.consume();
         });
         cancelButton.setOnAction(event -> {
-            handlers.forEach(handler -> {
-                handler.cancel(event);
-            });
+            handleCancel(event);
             event.consume();
         });
 
