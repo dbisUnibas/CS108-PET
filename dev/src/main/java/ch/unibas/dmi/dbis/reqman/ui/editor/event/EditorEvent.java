7be8ee3cca4edea7f70cfbe450e24d25f1bd8d83
sauterl <loris.sauter@gmail.com>
2017-02-15 11:37:35 +0100

    Introduced event system

---
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/editor/event/EditorEvent.java
@@ -0,0 +1,53 @@
+package ch.unibas.dmi.dbis.reqman.ui.editor.event;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventTarget;
+import javafx.event.EventType;
+
+/**
+ * TODO: write JavaDoc
+ *
+ * @author loris.sauter
+ */
+public class EditorEvent extends ActionEvent {
+
+    public static EventType<EditorEvent> CREATION = new EventType<>(ACTION, "creation");
+    public static EventType<EditorEvent> DELETION = new EventType<>(ACTION, "deletion");
+    public static EventType<EditorEvent> MODIFICATION = new EventType<>(ACTION, "modification");
+
+    private TargetEntity targetEntity;
+
+    private EventType<EditorEvent> eventType;
+
+    @Override
+    public EventType<? extends ActionEvent> getEventType(){
+        return  eventType == null ? ANY : eventType;
+    }
+
+    public EditorEvent(){
+        super();
+    }
+
+
+    public EditorEvent(Object source, EventTarget target, EventType<EditorEvent> type, TargetEntity targetEntity){
+        super(source, target);
+        this.eventType = type;
+        this.targetEntity = targetEntity;
+    }
+
+    public TargetEntity getTargetEntity() {
+        return targetEntity;
+    }
+
+    public static EditorEvent generateCreationEvent(Object source, EventTarget target, TargetEntity targetEntity){
+        return new EditorEvent(source, target, CREATION, targetEntity);
+    }
+
+    public static EditorEvent generateDeletionEvent(Object source, EventTarget target, TargetEntity targetEntity){
+        return new EditorEvent(source, target, DELETION, targetEntity);
+    }
+
+    public static EditorEvent generateModificationEvent(Object source, EventTarget target, TargetEntity targetEntity){
+        return new EditorEvent(source, target, MODIFICATION, targetEntity);
+    }
+}
---
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/editor/event/TargetEntity.java
@@ -0,0 +1,13 @@
+package ch.unibas.dmi.dbis.reqman.ui.editor.event;
+
+/**
+ * TODO: write JavaDoc
+ *
+ * @author loris.sauter
+ */
+public enum TargetEntity {
+
+    REQUIREMENT,
+    MILESTONE,
+    CATALOGUE;
+}
