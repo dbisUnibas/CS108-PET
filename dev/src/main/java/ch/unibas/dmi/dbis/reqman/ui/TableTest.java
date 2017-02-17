9c2d714696763d884d5651efbfe749d4ae480afa
sauterl <loris.sauter@gmail.com>
2017-02-17 09:53:27 +0100

    Tinkered around with tableviews

--- dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/TableTest.java
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/ui/TableTest.java
@@ -22,27 +22,42 @@ public class TableTest extends Application {
         launch(args);
     }
 
+    final ObservableList<MetaKeyValuePair> tableData = FXCollections.observableArrayList(
+            new MetaKeyValuePair("key", "value"),
+            new MetaKeyValuePair("category", "dump"),
+            new MetaKeyValuePair("foo", "bar")
+    );
+
     @Override
     public void start(Stage primaryStage) {
         BorderPane pane = new BorderPane();
         Scene scene = new Scene(pane);
 
-        ObservableList<KeyValuePropertyPair> tableData = FXCollections.observableArrayList();
-        tableData.add(new KeyValuePropertyPair("key", "value"));
 
-        TableView table = new TableView<>();
+        TableView<MetaKeyValuePair> table = new TableView<>();
         table.setEditable(true);
 
-        TableColumn keyCol = new TableColumn("Key");
-        keyCol.setCellValueFactory(new PropertyValueFactory<>("key"));
-        keyCol.setCellFactory(TextFieldTableCell.<KeyValuePropertyPair>forTableColumn() );
-
-        TableColumn valCol = new TableColumn("Value");
-        valCol.setCellValueFactory(new PropertyValueFactory<>("value"));
+        TableColumn<MetaKeyValuePair, String> firstCol = new TableColumn<>("Key");
+        firstCol.setCellValueFactory(
+                new PropertyValueFactory<>("key")
+        );
+        firstCol.setCellFactory(TextFieldTableCell.forTableColumn() );
+        firstCol.setOnEditCommit((TableColumn.CellEditEvent<MetaKeyValuePair, String> t)->{
+            t.getTableView().getItems().get(t.getTablePosition().getRow()).setKey(t.getNewValue());
+        });
+        TableColumn<MetaKeyValuePair, String> secondCol = new TableColumn<>("Value");
+        secondCol.setCellValueFactory(
+                new PropertyValueFactory<>("value")
+        );
+        secondCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        secondCol.setOnEditCommit((TableColumn.CellEditEvent<MetaKeyValuePair, String> t) ->{
+            t.getTableView().getItems().get(t.getTablePosition().getRow()).setValue(t.getNewValue());
+        });
 
         table.setItems(tableData);
-        table.getColumns().addAll(keyCol, valCol);
+        table.getColumns().addAll(firstCol,secondCol);
 
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         pane.setCenter(table);
 
 
@@ -51,44 +66,20 @@ public class TableTest extends Application {
         primaryStage.show();
     }
 
-    public static class KeyValuePropertyPair {
-        private SimpleStringProperty key;
-        private SimpleStringProperty value;
+    public static class MetaKeyValuePair{
+        private final SimpleStringProperty key;
 
-        public KeyValuePropertyPair(String key, String value) {
+        public MetaKeyValuePair(String key, String value) {
             this.key = new SimpleStringProperty(key);
             this.value = new SimpleStringProperty(value);
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            KeyValuePropertyPair that = (KeyValuePropertyPair) o;
-
-            if (getKey() != null ? !getKey().equals(that.getKey()) : that.getKey() != null) {
-                return false;
-            }
-            return getValue() != null ? getValue().equals(that.getValue()) : that.getValue() == null;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = getKey() != null ? getKey().hashCode() : 0;
-            result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
-            return result;
-        }
-
-        public KeyValuePropertyPair() {
-        }
-
         public String getKey() {
             return key.get();
+        }
+
+        public SimpleStringProperty keyProperty() {
+            return key;
         }
 
         public void setKey(String key) {
@@ -99,9 +90,15 @@ public class TableTest extends Application {
             return value.get();
         }
 
+        public SimpleStringProperty valueProperty() {
+            return value;
+        }
+
         public void setValue(String value) {
             this.value.set(value);
         }
+
+        private final SimpleStringProperty value;
     }
 
 
