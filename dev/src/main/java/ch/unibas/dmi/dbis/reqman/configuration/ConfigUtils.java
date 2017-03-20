646858c782bcaf097ea8b2698091af813c53cf8f
loris.sauter <loris.sauter@unibas.ch>
2017-03-20 22:18:57 +0100

    Formatted classes in package configuration

--- dev/src/main/java/ch/unibas/dmi/dbis/reqman/configuration/ConfigUtils.java
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/configuration/ConfigUtils.java
@@ -11,20 +11,20 @@ public class ConfigUtils {
 
     public static final String CONFIG_EXTENSION = "config";
 
-    private ConfigUtils(){
+    private ConfigUtils() {
         // No instance needed
     }
 
-    public static boolean isJARexecuted(){
+    public static boolean isJARexecuted() {
         String codeSourceLocation = ConfigUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         return codeSourceLocation.endsWith(".jar");
     }
 
-    public static File getCodeSourceLocation(){
-        return new File(ConfigUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath() );
+    public static File getCodeSourceLocation() {
+        return new File(ConfigUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath());
     }
 
-    public static final String getFileSeparator(){
+    public static final String getFileSeparator() {
         return System.getProperty("file.separator");
     }
 
