58d90d26d6f56b71f40719bedda342819b7d4953
sauterl <loris.sauter@gmail.com>
2017-03-31 16:13:42 +0200

    Wrote additional javadoc and removed the todo for it

--- dev/src/main/java/ch/unibas/dmi/dbis/reqman/common/StringUtils.java
+++ dev/src/main/java/ch/unibas/dmi/dbis/reqman/common/StringUtils.java
@@ -7,7 +7,8 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- * TODO: Write JavaDoc
+ * {@link StringUtils} contains random utily functions related to strings.
+ * This class is not designed to be instanciated and has no public constructor for that particular reason.
  *
  * @author loris.sauter
  */
@@ -73,7 +74,7 @@ public class StringUtils {
     }
 
     /**
-     * Prints a given timestamp with the SimpleDateFormat {@code dd.MM.YYYY HH:mm:ss:SSS}
+     * Prints a given timestamp with the {@link SimpleDateFormat} {@code dd.MM.YYYY HH:mm:ss:SSS}
      * @param timestamp The timestamp to pretty print
      * @return The formatted timestamp, as a string.
      */
@@ -81,6 +82,13 @@ public class StringUtils {
         return prettyPrintTimestamp(timestamp, "dd.MM.YYYY HH:mm:ss:SSS");
     }
 
+    /**
+     * Prints a given timestamp with the specified format.
+     * The format ist then used to setup a {@link SimpleDateFormat} which formats the timestamp.
+     * @param timestamp The timestamp to pretty print.
+     * @param format The format expression to pass to the {@link SimpleDateFormat} constructor.
+     * @return The formatted timestamp in its string representation
+     */
     public static String prettyPrintTimestamp(long timestamp, String format){
         Date d = new Date(timestamp);
         SimpleDateFormat sdf = new SimpleDateFormat(format);
