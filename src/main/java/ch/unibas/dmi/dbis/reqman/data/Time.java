c50656b2ec3d75e41a9fb83b55a979c8cad54cfb
loris.sauter <loris.sauter@unibas.ch>
2017-10-08 15:31:21 +0200

    Adding the time entity

---
+++ src/main/java/ch/unibas/dmi/dbis/reqman/data/Time.java
@@ -0,0 +1,62 @@
+package ch.unibas.dmi.dbis.reqman.data;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * TODO: write JavaDoc
+ *
+ * @author loris.sauter
+ */
+public class Time {
+  
+  private final UUID uuid;
+  
+  private Date date;
+  
+  public Time() {
+    this.uuid = UUID.randomUUID();
+  }
+  
+  public Time(Date date) {
+    this();
+    this.date = date;
+  }
+  
+  public Date getDate() {
+    return date;
+  }
+  
+  public void setDate(Date date) {
+    this.date = date;
+  }
+  
+  public UUID getUuid() {
+    return uuid;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    
+    Time time = (Time) o;
+    return getUuid().equals(time.getUuid());
+  }
+  
+  @Override
+  public int hashCode() {
+    int result = getUuid() != null ? getUuid().hashCode() : 0;
+    result = 31 * result + (getDate() != null ? getDate().hashCode() : 0);
+    return result;
+  }
+  
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("Time{");
+    sb.append("uuid=").append(uuid);
+    sb.append(", date=").append(date);
+    sb.append('}');
+    return sb.toString();
+  }
+}
