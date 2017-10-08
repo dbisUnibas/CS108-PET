7f6ea5621e03a3a177f844b7c42dd5f264209502
loris.sauter <loris.sauter@unibas.ch>
2017-10-08 15:27:49 +0200

    Introducing an UUID for each entity
    * Replaced `toString`, `hashCode` and `equals` to be UUID sensitive
    * `equals` now only considers the UUIIntroducing an UUID for each entity

--- src/main/java/ch/unibas/dmi/dbis/reqman/data/Milestone.java
+++ src/main/java/ch/unibas/dmi/dbis/reqman/data/Milestone.java
@@ -1,6 +1,7 @@
 package ch.unibas.dmi.dbis.reqman.data;
 
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * TODO: write JavaDoc
@@ -8,77 +9,83 @@ import java.util.Date;
  * @author loris.sauter
  */
 public class Milestone {
-
-    private int ordinal = 0;
-
-    private String name;
-
-    private Date date;
-
-    public Milestone() {
-
-    }
-
-    public Milestone(String name, int ordinal, Date date) {
-        this.name = name;
-        this.ordinal = ordinal;
-        this.date = date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Milestone milestone = (Milestone) o;
-
-        return this.getOrdinal() == milestone.getOrdinal();
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getOrdinal();
-        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
-        result = 31 * result + (getDate() != null ? getDate().hashCode() : 0);
-        return result;
-    }
-
-    public int getOrdinal() {
-        return ordinal;
-    }
-
-    // TODO reduce visibility
-    public void setOrdinal(int ordinal) {
-        this.ordinal = ordinal;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Milestone{");
-        sb.append("ordinal=").append(ordinal);
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", date=").append(date);
-        sb.append('}');
-        return sb.toString();
-    }
+  
+  private final UUID uuid;
+  
+  private int ordinal = 0;
+  
+  private String name;
+  
+  private Date date;
+  
+  public Milestone() {
+    uuid = UUID.randomUUID();
+  }
+  
+  public Milestone(String name, int ordinal, Date date) {
+    this();
+    this.name = name;
+    this.ordinal = ordinal;
+    this.date = date;
+  }
+  
+  
+  public int getOrdinal() {
+    return ordinal;
+  }
+  
+  // TODO reduce visibility
+  public void setOrdinal(int ordinal) {
+    this.ordinal = ordinal;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public Date getDate() {
+    return date;
+  }
+  
+  public void setDate(Date date) {
+    this.date = date;
+  }
+  
+  
+  public UUID getUuid() {
+    return uuid;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    
+    Milestone milestone = (Milestone) o;
+    return getUuid().equals(milestone.getUuid());
+  }
+  
+  @Override
+  public int hashCode() {
+    int result = getUuid() != null ? getUuid().hashCode() : 0;
+    result = 31 * result + getOrdinal();
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+    result = 31 * result + (getDate() != null ? getDate().hashCode() : 0);
+    return result;
+  }
+  
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("Milestone{");
+    sb.append("uuid=").append(uuid);
+    sb.append(", ordinal=").append(ordinal);
+    sb.append(", name='").append(name).append('\'');
+    sb.append(", date=").append(date);
+    sb.append('}');
+    return sb.toString();
+  }
 }
--- src/main/java/ch/unibas/dmi/dbis/reqman/data/ProgressSummary.java
+++ src/main/java/ch/unibas/dmi/dbis/reqman/data/ProgressSummary.java
@@ -1,77 +1,88 @@
 package ch.unibas.dmi.dbis.reqman.data;
 
+import java.util.UUID;
+
 /**
  * TODO: write JavaDoc
  *
  * @author loris.sauter
  */
 public class ProgressSummary {
-
-    private int milestoneOrdinal;
-    private String internalComment;
-    private String externalComment;
-
-    public ProgressSummary(int milestoneOrdinal, String internalComment, String externalComment) {
-
-        this.milestoneOrdinal = milestoneOrdinal;
-        this.internalComment = internalComment;
-        this.externalComment = externalComment;
-    }
-
-    public ProgressSummary() {
-
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        ProgressSummary that = (ProgressSummary) o;
-
-        if (getMilestoneOrdinal() != that.getMilestoneOrdinal()) {
-            return false;
-        }
-        if (getInternalComment() != null ? !getInternalComment().equals(that.getInternalComment()) : that.getInternalComment() != null) {
-            return false;
-        }
-        return getExternalComment() != null ? getExternalComment().equals(that.getExternalComment()) : that.getExternalComment() == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getMilestoneOrdinal();
-        result = 31 * result + (getInternalComment() != null ? getInternalComment().hashCode() : 0);
-        result = 31 * result + (getExternalComment() != null ? getExternalComment().hashCode() : 0);
-        return result;
-    }
-
-    public int getMilestoneOrdinal() {
-
-        return milestoneOrdinal;
-    }
-
-    public void setMilestoneOrdinal(int milestoneOrdinal) {
-        this.milestoneOrdinal = milestoneOrdinal;
-    }
-
-    public String getInternalComment() {
-        return internalComment;
-    }
-
-    public void setInternalComment(String internalComment) {
-        this.internalComment = internalComment;
-    }
-
-    public String getExternalComment() {
-        return externalComment;
-    }
-
-    public void setExternalComment(String externalComment) {
-        this.externalComment = externalComment;
-    }
+  
+  private final UUID uuid;
+  
+  private int milestoneOrdinal;
+  private String internalComment;
+  private String externalComment;
+  
+  public ProgressSummary(int milestoneOrdinal, String internalComment, String externalComment) {
+    this();
+    this.milestoneOrdinal = milestoneOrdinal;
+    this.internalComment = internalComment;
+    this.externalComment = externalComment;
+  }
+  
+  public ProgressSummary() {
+    this.uuid = UUID.randomUUID();
+  }
+  
+  
+  public int getMilestoneOrdinal() {
+    
+    return milestoneOrdinal;
+  }
+  
+  public void setMilestoneOrdinal(int milestoneOrdinal) {
+    this.milestoneOrdinal = milestoneOrdinal;
+  }
+  
+  public String getInternalComment() {
+    return internalComment;
+  }
+  
+  public void setInternalComment(String internalComment) {
+    this.internalComment = internalComment;
+  }
+  
+  public String getExternalComment() {
+    return externalComment;
+  }
+  
+  public void setExternalComment(String externalComment) {
+    this.externalComment = externalComment;
+  }
+  
+  public UUID getUuid() {
+    return uuid;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    
+    ProgressSummary that = (ProgressSummary) o;
+    
+    return getUuid().equals(that.getUuid());
+  }
+  
+  @Override
+  public int hashCode() {
+    int result = getUuid().hashCode();
+    result = 31 * result + getMilestoneOrdinal();
+    result = 31 * result + (getInternalComment() != null ? getInternalComment().hashCode() : 0);
+    result = 31 * result + (getExternalComment() != null ? getExternalComment().hashCode() : 0);
+    return result;
+  }
+  
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("ProgressSummary{");
+    sb.append("uuid=").append(uuid);
+    sb.append(", milestoneOrdinal=").append(milestoneOrdinal);
+    sb.append(", internalComment='").append(internalComment).append('\'');
+    sb.append(", externalComment='").append(externalComment).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
 }
