5f2f10333953061a3a3b4bcf04ad2fa210755043
Silvan Heller <silvan.heller@unibas.ch>
2019-01-02 15:11:16 +0100

    Upgrading to Java 11 Introducing a separate main class to work around a bug in openjfx & gradle on mac

---
+++ src/main/java/ch/unibas/dmi/dbis/reqman/main/Main.java
@@ -0,0 +1,11 @@
+package ch.unibas.dmi.dbis.reqman.main;
+
+import ch.unibas.dmi.dbis.reqman.ui.ReqmanApplication;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ReqmanApplication.main(args);
+  }
+
+}
